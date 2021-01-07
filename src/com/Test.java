cf38c18925a530e68dc873858ff96daec4f43201
峥峥 <1773966022.com>
2021-01-07 19:39:45 +0800

    第一次

---
+++ src/com/Test.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author 武爸爸
+ * @create 2021-01-07 19:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("555");
+    }
+}
